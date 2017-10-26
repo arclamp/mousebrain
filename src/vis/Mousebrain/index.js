4e16873e5878be85e9ab6b641119789a4c1787b1
Roni Choudhury <roni.choudhury@kitware.com>
2017-10-26 13:34:25 -0400

    Add drilldown marker to epoch viewer

--- src/vis/Mousebrain/index.js
+++ src/vis/Mousebrain/index.js
@@ -6,12 +6,17 @@ import LineChart from 'candela/plugins/vega/LineChart';
 
 import template from './index.jade';
 
+function clamp(val, low, high) {
+  return val < low ? low : (val > high ? high : val);
+}
+
 class EpochChart extends VisComponent {
   constructor (el, options) {
     super(el);
 
     // Capture a sorted version of the epoch data.
     this.epochs = [...options.data].sort((a, b) => a.Epoch - b.Epoch);
+    this.frames = this.epochs[this.epochs.length - 1].End;
 
     // Establish a colormap object.
     this.cmap = scaleOrdinal(schemeDark2);
@@ -19,6 +24,9 @@ class EpochChart extends VisComponent {
     // Make the top-level div a flex box.
     select(this.el)
       .style('display', 'flex');
+
+    // Initialize drilldown marker.
+    this.drilldown = 0;
   }
 
   render () {
@@ -27,7 +35,6 @@ class EpochChart extends VisComponent {
       .getBoundingClientRect()
       .width;
 
-    const frames = this.epochs[this.epochs.length - 1].End;
 
     let sel = select(this.el)
       .selectAll('div.frac')
@@ -37,8 +44,26 @@ class EpochChart extends VisComponent {
       .append('div')
       .classed('frac', true)
       .merge(sel)
-      .style('width', d => `${(d.End - d.Start) * width / frames}px`)
+      .style('width', d => `${(d.End - d.Start) * width / this.frames}px`)
       .style('background', d => this.cmap(d.Epoch));
+
+    sel = select(this.el)
+      .selectAll('div.marker')
+      .data([this.drilldown]);
+
+    sel.enter()
+      .append('div')
+      .classed('marker', true)
+      .style('position', 'relative')
+      .style('height', '30px')
+      .style('width', '2px')
+      .style('background', 'black')
+      .merge(sel)
+      .style('left', d => `${(d - this.frames) * width / this.frames}px`);
+  }
+
+  setDrilldown (frame) {
+    this.drilldown = clamp(frame, 0, this.frames);
   }
 }
 
@@ -52,6 +77,7 @@ export class Mousebrain extends VisComponent {
     select(this.el)
       .html(template());
 
+    // Initialize the data display.
     const dataDiv = select(this.el)
       .select('div.data')
       .node();
@@ -64,6 +90,7 @@ export class Mousebrain extends VisComponent {
       height: height - 30
     });
 
+    // Initialize the epoch display.
     this.epochDiv = select(this.el)
       .select('div.epoch')
       .node();
@@ -75,10 +102,19 @@ export class Mousebrain extends VisComponent {
     this.epochVis = new EpochChart(this.epochDiv, {
       data: options.epochData
     });
+
+    // Initialize the drilldown marker.
+    this.drilldown = 0;
   }
 
   render () {
     this.dataVis.render();
     this.epochVis.render();
+  }
+
+  setDrilldown (frame) {
+    this.drilldown = frame;
+
+    this.epochVis.setDrilldown(frame);
   }
 }
